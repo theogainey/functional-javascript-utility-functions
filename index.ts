b49a9c29d997be52d98db6e5c096f2ad91e02266
Theo Gainey <theo@heysparkbox.com>
2022-05-15 12:28:01 -0400

    fea: typescript

---
+++ index.ts
@@ -0,0 +1,25 @@
+export const pipe = (...functions: Function[]) =>
+  (x: any) =>
+    functions.reduce(
+      (acc, fn) => fn(acc),
+      x,
+    );
+
+export const pipeCompatibleSplit = (separator: string) =>
+  (target: string) => target.split(separator);
+
+export const pipeCompatibleMap = <T1, T2>(callback: (x: T1) => T2) =>
+  (target: Array<T1>) => target.map(callback);
+
+export const pipeCompatibleFilter = <T>(callback: (x: T) => boolean) =>
+  (target: Array<T>) => target.filter(callback);
+
+export const pipeCompatibleReduce = <T1, T2>(
+  callback: (
+    previousValue: T2,
+    currentValue: T1,
+    currentIndex: number,
+    array: T1[],
+  ) => T2,
+) =>
+  (initValue: T2) => (target: Array<T1>) => target.reduce(callback, initValue);
